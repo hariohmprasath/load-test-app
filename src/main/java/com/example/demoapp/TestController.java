1fdf6d4e71ce73a6efbb6b6c77b9c6ccebdb5648
Hari Ohm Prasth Rajagopal <hariohmprasath@gmail.com>
2023-08-20 02:44:22 -0700

    Updates for error code

--- src/main/java/com/example/demoapp/TestController.java
+++ src/main/java/com/example/demoapp/TestController.java
@@ -1,7 +1,7 @@
 package com.example.demoapp;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +11,9 @@ public class TestController {
             ? 10 : Integer.parseInt(System.getenv("MAX_REQUESTS"));
 
     @GetMapping
-    public @ResponseBody String all() {
-        if (MetricEmitter.COUNTER >= MAX_REQUESTS) {
-            throw new RuntimeException("Too many requests");
-        }
+    public ResponseEntity<String> all() {
+        if (MetricEmitter.COUNTER >= MAX_REQUESTS) 
+            return ResponseEntity.status(429).body("Too many requests");        
         // Increment the counter
         MetricEmitter.increment();
         final double total = MetricEmitter.COUNTER;
@@ -27,6 +26,6 @@ public class TestController {
         }
         // Decrement the counter
         MetricEmitter.decrement();
-        return String.valueOf(total);
+        return ResponseEntity.ok(String.valueOf(total));
     }
 }
